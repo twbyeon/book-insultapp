12355cc2bd2ca0d43468670bfeb483b47acff2a3
twbyeon <39757981+twbyeon@users.noreply.github.com>
2018-06-01 16:02:11 +0900

    sdfsd

--- src/main/java/org/openshift/InsultResource.java
+++ src/main/java/org/openshift/InsultResource.java
@@ -1,21 +1,17 @@
 package org.openshift;
-
 import java.util.HashMap;
 import javax.enterprise.context.RequestScoped;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-
 @RequestScoped
-@Path("/insert")
+@Path("/insult")
 public class InsultResource {
-	
-	@GET()
-	@Produces("application/json")
-	public HashMap<String,String> getInsult() {
-		HashMap<String, String> theInsult = new HashMap<String, String>();
-		theInsult.put("insult", new InsultGenerator().generateInsult());
-
-		return theInsult;
-	}
-} 
+@GET()
+@Produces("application/json")
+public HashMap<String,String> getInsult() {
+HashMap<String, String> theInsult = new HashMap<String, String>();
+theInsult.put("insult", new InsultGenerator().generateInsult());
+return theInsult;
+}
+}
